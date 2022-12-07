0dda74db83259ff67c50df276932e1074b2fed71
Raunak Pradip Shah <raunakshah13@gmail.com>
2022-12-07 14:50:00 +0530

    refactor and run curl only if not in pwd

--- prow.sh
+++ prow.sh
@@ -786,7 +786,6 @@ install_snapshot_crds() {
 
 # Enable feature flags in the snapshot-controller.
 enable_snapshot_controller_feature_flags () {
-  SNAPSHOT_CONTROLLER_YAML="${CSI_PROW_WORK}/snapshot-controller.yaml"
   if volume_mode_conversion; then
     echo "Deploying snapshot-controller with --prevent-volume-mode-conversion=true"
     sed -i -e 's/# end snapshot controller args/- \"--prevent-volume-mode-conversion=true\"\n            # end snapshot controller args/' "${SNAPSHOT_CONTROLLER_YAML}"
@@ -795,11 +794,16 @@ enable_snapshot_controller_feature_flags () {
 
 # Install snapshot controller and associated RBAC, retrying until the pod is running.
 install_snapshot_controller() {
-  CONTROLLER_DIR="https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${CSI_SNAPSHOTTER_VERSION}"
   if [[ ${REPO_DIR} == *"external-snapshotter"* ]]; then
-      CONTROLLER_DIR="${REPO_DIR}"
+      SNAPSHOT_RBAC_YAML="${REPO_DIR}/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml"
+      SNAPSHOT_CONTROLLER_YAML="${REPO_DIR}/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml"
+  else
+      CONTROLLER_DIR="https://raw.githubusercontent.com/kubernetes-csi/external-snapshotter/${CSI_SNAPSHOTTER_VERSION}"
+      SNAPSHOT_RBAC_YAML="${CONTROLLER_DIR}/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml"
+      run curl "${CONTROLLER_DIR}/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml" --output "${CSI_PROW_WORK}/snapshot-controller.yaml" --silent --location
+      SNAPSHOT_CONTROLLER_YAML="${CSI_PROW_WORK}/snapshot-controller.yaml"
   fi
-  SNAPSHOT_RBAC_YAML="${CONTROLLER_DIR}/deploy/kubernetes/snapshot-controller/rbac-snapshot-controller.yaml"
+
   echo "kubectl apply -f ${SNAPSHOT_RBAC_YAML}"
   # Ignore: Double quote to prevent globbing and word splitting.
   # shellcheck disable=SC2086
@@ -818,8 +822,6 @@ install_snapshot_controller() {
     sleep 10
   done
 
-  run curl "${CONTROLLER_DIR}/deploy/kubernetes/snapshot-controller/setup-snapshot-controller.yaml" --output "${CSI_PROW_WORK}/snapshot-controller.yaml" --silent --location
-  SNAPSHOT_CONTROLLER_YAML="${CSI_PROW_WORK}/snapshot-controller.yaml"
   enable_snapshot_controller_feature_flags || die "failed to enable snapshot-controller feature flags"
 
   if [[ ${REPO_DIR} == *"external-snapshotter"* ]]; then
